3d595a59af2b49566c61d63b2621831dd4efba33
igel <igel@piratemesh.net>
2013-09-01 19:41:30 -0400

    [meshbox-openwrt/luci-cjdns] Cleaned up var naming conv, Added pasteblock section for quick adding from suggested ezcrypt site, documented the options for beacons (in list)

--- luci-cjdns/luasrc/model/cbi/cjdns/cjdns.lua
+++ luci-cjdns/luasrc/model/cbi/cjdns/cjdns.lua
@@ -14,17 +14,75 @@ $Id: cjdns.lua 9570 2012-12-25 02:45:42Z jow $
 ]]--
 
 m = Map("cjdns", luci.util.pcdata(translate("Project Meshnet")),
-	translate("cjd's ideology that networks should be easy to set up, protocols should scale up smoothly, and security should be ubiquitous."))
+	translate("cjd's ideology that networks should be easy to set up, \
+				protocols should scale up smoothly, \
+					and security should be ubiquitous."))
 
 m:section(SimpleSection).template  = "cjdns_status"
 
-s = m:section(NamedSection, "config", "cjdns", translate("cjdns settings"))
+---------------------
+-- Cjdns Node List --
+---------------------
+nodemgmt = m:section(TypedSection, "node", translate("Cjdns Node List"),
+	translate("First find the Cjdns ip of your upstream node. \
+		(Ask him/her if you can't find out) This is the node you got connection \
+		credentials from."))
+
+nodemgmt.anonymous = true
+nodemgmt.addremove = true
+nodemgmt.template  = "cbi/tblsection"
+
+-- Name --
+nodemgmt:option(Value, "name", translate("Name"))
+nodemgmt.placeholder = ""
+
+-- Address --
+ia = nodemgmt:option(Value, "address", translate("Address"))
+ia.datatype    = "ipaddr" -- return ip4addr(val) or ip6addr(val)
+ia.placeholder = ""
+
+-- Port --
+ep = nodemgmt:option(Value, "port", translate("Port"))
+ep.datatype    = "portrange"
+ep.placeholder = ""
+
+-- Public Key --
+ip = nodemgmt:option(Value, "pubkey", translate("Public Key"))
+ip.datatype    = "string"
+ip.placeholder = ""
+
+-- Password --
+ac = nodemgmt:option(Value, "secret", translate("Password"))
+ac.datatype    = "string"
+ac.placeholder = ""
+
+-- TODO
+-- Node(s) to connect to manually.
+-- ETHInterface -> connectTo -> "01:02:03:04:05:06":{"password":"a","publicKey":"b"}
+
+
+----------------------
+-- Cjdns   Settings --
+----------------------
+s = m:section(NamedSection, "config", "Cjdns Settings", translate("Cjdns Settings"))
 s.addremove = false
+
+----------
+-- Tabs --
+----------
 s:tab("general",  translate("General Settings"))
 s:tab("advanced", translate("Advanced Settings"))
+s:tab("EzPaste",  translate("Easy Paste Node"))
 
-e = s:taboption("general", Flag, "_init", translate("Start Cjdns and NAT66 service"))
+----------------------
+-- General Settings --
+----------------------
+
+e = s:taboption("general", Flag, "_init", translate("Enable Cjdns and NAT66 service"),
+	translate("Toggles the start of both Cjdns and NAT66 services upon Boot"))
+e.default  = 1
 e.rmempty  = false
+
 
 function e.cfgvalue(self, section)
 	return luci.sys.init.enabled("cjdns") and self.enabled or self.disabled
@@ -40,78 +98,70 @@ function e.write(self, section, value)
 	end
 end
 
-s:taboption("general", Flag, "enable_cjdns", translate("Enable Cjdns functionality")).default = "1"
-s:taboption("general", Flag, "enable_nat66", translate("Enable NAT66 functionality")).default = "1"
+-------------
+-- Beacons --
+-------------
+bc = s:taboption("general", ListValue, "enable_beacons", translate("Enable Beacons"),
+	translate("Select the preferred Beacons mode for ETHInterface"))
+bc:value(0, translate("0 -- Disabled."))
+bc:value(1, translate("1 -- Accept Beacons, this will cause Cjdns to accept incoming \
+                      Beacon messages and try connecting to the Sender."))
+bc:value(2, translate("2 -- Accept and Send Beacons to LAN broadcast address which \
+                      contain a One-time Pad secret password."))
 
-s:taboption("general", Flag, "secure_mode", translate("Enable Beacons"),
-	translate("Allow Beacons set to 2")).default = "1"
+-------------
+-- Logging --
+-------------
+s:taboption("general", Flag, "enable_stdlog", translate("Enable additional logging to logread"),
+	translate("Puts extra debugging information into logread"))
 
-s:taboption("general", Flag, "log_output", translate("Enable additional logging to logread"),
-	translate("Puts extra debugging information into logread (which can be redirected to remote syslog-ng)"))
+-- s:taboption("general", Value, "cfg_pubkey", translate("Public Key"),
+	-- translate("Your Multipass to Hyperboria")).rmempty = true
 
-s:taboption("general", Value, "download", translate("Public Key"),
-	translate("Your Multipass to Hyperboria")).rmempty = true
+-- s:taboption("general", Value, "cfg_prvkey", translate("Private Key"),
+	-- translate("Do not redistribute this key")).rmempty = true
 
-s:taboption("general", Value, "upload", translate("Private Key"),
-	translate("Do not redistribute this key")).rmempty = true
-
-port = s:taboption("general", Value, "port", translate("Port"))
-port.datatype = "port"
+-- port = s:taboption("general", Value, "port", translate("Port"))
+-- port.datatype = "port"
 -- port.default  = 5000
 
+-----------------------
+-- Advanced Settings --
+-----------------------
 
-s:taboption("advanced", Flag, "system_uptime", translate("Report system instead of daemon uptime")).default = "1"
-
-s:taboption("advanced", Value, "uuid", translate("Advanced Setting 1"))
+s:taboption("advanced", Value, "uuid",          translate("Advanced Setting 1"))
 s:taboption("advanced", Value, "serial_number", translate("Advanced Setting 2"))
-s:taboption("advanced", Value, "model_number", translate("Advanced Setting 3"))
+s:taboption("advanced", Value, "model_number",  translate("Advanced Setting 3"))
 
 ni = s:taboption("advanced", Value, "notify_interval", translate("Advanced Setting 4"))
 ni.datatype    = "uinteger"
 ni.placeholder = 30
 
-ct = s:taboption("advanced", Value, "clean_ruleset_threshold", translate("Advanced Setting 5"))
-ct.datatype    = "uinteger"
-ct.placeholder = 20
-
-ci = s:taboption("advanced", Value, "clean_ruleset_interval", translate("Advanced Setting 6"))
-ci.datatype    = "uinteger"
-ci.placeholder = 600
-
-pu = s:taboption("advanced", Value, "presentation_url", translate("Advanced Setting 7"))
-pu.placeholder = "http://[fc3g:31d4:3g2d:24tf:4g32:4g23:42d2]/"
-
 lf = s:taboption("advanced", Value, "cjdns_lease_file", translate("Advanced Setting 8"))
 lf.placeholder = "/var/log/cjdns.leases"
 
+----------------------
+--> Easy Paste Node --
+----------------------
+ez = s:taboption("EzPaste", Value, "EzPaste",
+	             translate("Node information should be relayed by \
+                           an Encrypted Pastebin: EZCrypt https://ezcrypt.it/"))
+ez.template = "cbi/tvalue"
+ez.rows     = 5
+ez.datatype = "string"
 
-s2 = m:section(TypedSection, "perm_rule", translate("Cjdns Node List"),
-	translate("First find the Cjdns ip of your upstream node. \
-		(Ask him/her if you can't find out) This is the node you got connection \
-		credentials from."))
+----------------------
+--> Generate Config --
+----------------------
+-- Button to auto-generate EzCrypt URL with routers /etc/cjdroute.conf info would be nice.
 
-s2.template  = "cbi/tblsection"
--- s2.sortable  = true
-s2.anonymous = true
-s2.addremove = true
--- Node	Cjdns IP	Latency	Other	Status
-s2:option(Value, "comment", translate("Node"))
-s2.placeholder = "Igel_VPS"
 
-ep = s2:option(Value, "ext_ports", translate("Cjdroute Port"))
-ep.datatype    = "portrange"
-ep.placeholder = "0-65535"
 
-ia = s2:option(Value, "int_addr", translate("Node IPv4"))
-ia.datatype    = "ip4addr"
-ia.placeholder = "0.0.0.0"
 
-ip = s2:option(Value, "int_ports", translate("Cjdns Public Key"))
-ip.datatype    = "portrange"
-ip.placeholder = "...hhj3hk.k"
 
-ac = s2:option(ListValue, "action", translate("Cjdroute Flow"))
-ac:value("incoming")
-ac:value("outgoing")
+
+
+
+
 
 return m
