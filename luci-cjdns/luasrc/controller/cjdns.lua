e5d87c87230cd1eddb77f96769387fe7f2ec3eef
igel <igel@piratemesh.net>
2013-11-10 23:44:56 -0500

    [meshbox-openwrt/luci-cjdns] Router IPTunnel added to Active cjdns nodes  * controller/cjdns.lua: bugfix when section null. new bug where option int is ignored by js.  * view/cjdns_status.htm: added ipt, renamed others according to conf.

--- luci-cjdns/luasrc/controller/cjdns.lua
+++ luci-cjdns/luasrc/controller/cjdns.lua
@@ -66,11 +66,9 @@ function act_status()
 		cjdstatus = { } -- print via luci.http.write_json
 		local udpif = conf.interfaces.UDPInterface[i]
 
-		if (udpif == nil) then
-			break
-		end
-		
-		for w,x in pairs(udpif.connectTo) do
+		if (udpif.connectTo ~= nil) then
+
+		 for w,x in pairs(udpif.connectTo) do
 			local num     = i -- used for act_delete(num)
 			local node    = w -- cfg035387 (anonymous uci field (.name))
 			local name    = x.name
@@ -80,7 +78,7 @@ function act_status()
 			local address = x.address
 			local latency = "Pinging..." 	-- TODO
 			local cjdnsip = "Resolving..." 	-- TODO
-			local other   = 1			-- TODO
+			local other   = 0
 
 			if address then -- and num and name and node and pubkey and passwd and other and port then
 				-- Fill in the tables fields and allow XHR.poll to refresh on 5s intervals.
@@ -92,7 +90,8 @@ function act_status()
 					other   = other,
 				}
 			end
-		end -- end for pairs
+		 end -- end for pairs
+		end -- end for sanity
 	end -- for conf.interfaces.UDPInterface{}
 
 	--[[
@@ -107,22 +106,21 @@ function act_status()
 	]]--
 
 	for i = 1,#conf.interfaces.ETHInterface do
-		-- local cjdstatus = { } -- print via luci.http.write_json
-		local ethif = conf.interfaces.ETHInterface[i]
-		if (ethif == nil) then
-			break
-		end
 
-		for w,x in pairs(ethif.connectTo) do
+		local ethif = conf.interfaces.ETHInterface[i]
+
+		if (ethif.connectTo ~= nil) then
+
+		 for w,x in pairs(ethif.connectTo) do
 			local num     = i
-			local node    = w
 			local name    = x.name
 			local pubkey  = x.publicKey
 			local passwd  = x.password
 			local address = w -- mac address
+			local node    = address
 			local latency = "Pinging..."
 			local cjdnsip = "Resolving..."
-			local other   = 0
+			local other   = 1
 
 			if address then
 				cjdstatus[#cjdstatus+1] = {
@@ -133,8 +131,8 @@ function act_status()
 					other   = other,
 				}
 			end
-		end -- end for pairs
-
+		 end -- end for pairs
+		end -- end for sanity
 	end -- for conf.interfaces.ETHInterface{}
 	--[[
 	8888888 8888888b.       88888888888                                  888
@@ -146,34 +144,71 @@ function act_status()
 	  888   888                 888  Y88b 888 888  888 888  888 Y8b.     888
 	8888888 888                 888   "Y88888 888  888 888  888  "Y8888  888
 	]]--
-	-- for i = 1,#conf.router.ipTunnel do
-	-- 	-- local cjdstatus = { } -- print via luci.http.write_json
-	-- iptun =  conf.router.ipTunnel
-	-- iptun2 = conf.router.ipTunnel.outgoingConnections 	-- 3
-	-- iptun3 = conf.router.ipTunnel.allowedConnections 	-- 2
-					-- name 	= "Break_name",
-					-- node    = "Break_address" .. #iptun, 	-- 0
-					-- cjdnsip = "Break_cjdnsip" .. #iptun2,  	-- 3
-					-- latency = "Break_latency" .. #iptun3, 	-- 2
-					-- other   = "Break_other",
 
+	for i = 1,#conf.router.ipTunnel do
 
-	-- for i = 1,#conf.router.ipTunnel do
-		-- local cjdstatus = { } -- print via luci.http.write_json
-		-- local tunnelout = conf.router.ipTunnel.outgoingConnections[1]
-		-- if (tunnelout == nil) then
-		-- #conf.router.ipTunnel.outgoingConnections,
-		-- if tunnelout then
-		-- 		cjdstatus[#cjdstatus+1] = {
-		-- 			name 	= tunnelout,
-		-- 			node    = tunnelout.ip4address,
-		-- 			cjdnsip = tunnelout.publicKey,
-		-- 			other   = 3,
-		-- 			latency = "Pinging...",
-		-- 			cjdnsip = "Resolving...",
-		-- 		}
-		-- 	-- break
-		-- end
+		local iptif = conf.router.ipTunnel[i]
+
+		if (iptif.allowedConnections ~= nil) then
+
+		 for w,x in pairs(iptif.allowedConnections) do
+			local name 		 = x.name
+			local publicKey  = x.publicKey
+			local ip4Address = x.ip4Address
+			local ip6Address = x.ip6Address
+			local latency 	 = "Pinging..."
+			local cjdnsip 	 = "Resolving..."
+			local other   	 = 2  -- BUG being duped for all below. how?
+			local y 		 = "|"
+
+			local address 	 = "Resolving..."
+			-- TODO check if not valid ip, ip6,
+			-- 	then check how to resolve domainname, or /etc/hosts
+
+			-- Display address Order by both, v4, v6, unresolved.
+			if ip4Address and ip6Address  then
+				address = ip4Address .. y .. ip6Address
+			elseif
+				(ip4Address ~= nil) then address = ip4Address
+			else
+				address = ip6Address
+			end
+
+			if publicKey then -- and (ip4Address or ip6Address) then
+				cjdstatus[#cjdstatus+1] = {
+					name 	= name,
+					node    = address,
+					cjdnsip = cjdnsip,
+					latency = latency,
+					other   = other,
+				}
+			end
+		 end -- end for pairs
+		end -- end for sanity
+
+		if (iptif.outgoingConnections ~= nil) then
+
+		 for w,x in pairs(iptif.outgoingConnections) do
+			local name 		 = x.name
+			local publicKey  = x.publicKey
+			local other   	 = 3
+			local latency 	 = "Pinging..."
+			local cjdnsip 	 = "Resolving..."
+			local address 	 = "Resolving..."
+
+			if publicKey then -- and (ip4Address or ip6Address) then
+				cjdstatus[#cjdstatus+1] = {
+					name 	= name,
+					node    = publicKey,
+					cjdnsip = cjdnsip,
+					latency = latency,
+					other   = other,
+				}
+			end
+		 end -- end for pairs
+		end -- end for sanity
+
+	end -- for conf.router.ipTunnel{}
 
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(cjdstatus)
