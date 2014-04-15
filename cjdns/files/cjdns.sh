72022dd355be71a894f2d7d2b85662fd3963dcba
igel <igel@piratemesh.net>
2014-04-15 04:04:43 +0200

    [meshbox-openwrt/cjdns] *CLEANUP* init script tabs spaces etc..

--- cjdns/files/cjdns.sh
+++ cjdns/files/cjdns.sh
@@ -21,52 +21,53 @@ PID="$(pgrep -f $CJDROUTE)"
 
 nat6()
 {
-    [ "$1" == "on" ] \
-	&& H="A" \
-	|| H="D"
-    
-    ip6tables -t nat    -${H} POSTROUTING -o tun0 -j MASQUERADE
-    ip6tables -t filter -${H} FORWARD -i tun0 -o br-lan -m state --state RELATED,ESTABLISHED -j ACCEPT
-    ip6tables -t filter -${H} FORWARD -i eth1 -o br-lan -j ACCEPT
+	[ "$1" == "on" ] \
+		&& H="A" \
+		|| H="D"
+
+	ip6tables -t nat    -${H} POSTROUTING -o tun0 -j MASQUERADE
+	ip6tables -t filter -${H} FORWARD -i tun0 -o br-lan -m state --state RELATED,ESTABLISHED -j ACCEPT
+	ip6tables -t filter -${H} FORWARD -i eth1 -o br-lan -j ACCEPT
 
 }
 stop()
 {
-    if [ -z "$PID" ]; then
-        echo "cjdns is not running"
-        return 1
-    else
-	for k in $PID
-	    do echo "cjdns (pid ${k}) terminated"
-	    kill -9 ${k}
-	done
-	nat6 off
-    fi
+	if [ -z "$PID" ]; then
+		echo "cjdns is not running"
+		return 1
+	else
+		for k in $PID
+			do echo "cjdns (pid ${k}) terminated"
+			kill -9 ${k}
+		done
+		nat6 off
+	fi
 }
 
 start()
 {
-    if [ ! -f $CONF ]; then
-        logger -t cjdns "No file found at $CONF creating new one" 
-        $CJDROUTE --genconf > /tmp/cjdns.tmp
-        $CJDROUTE --cleanconf < /tmp/cjdns.tmp > $CONF
-	rm /tmp/cjdns.tmp
-        lua /usr/share/cjdroutesetup.lua
-	/usr/share/cjdns_jsonpath.sh
-	sync
-    fi
+	if [ ! -f $CONF ]; then
+		logger -t cjdns "No file found at $CONF creating new one"
+		$CJDROUTE --genconf > /tmp/cjdns.tmp
+		$CJDROUTE --cleanconf < /tmp/cjdns.tmp > $CONF
+		rm /tmp/cjdns.tmp
+		lua /usr/share/cjdroutesetup.lua
+		/usr/share/cjdns_jsonpath.sh
+		sync
+	fi
 
-    if [ -z "$PID" ]; then
-        logger -t cjdns "Starting cjdns"
-        $CJDROUTE < $CONF
-	nat6 on
-        if [ $? -gt 0 ]; then
-            echo "Failed to start cjdns"
-            nat6 off
-	    return 1
-        fi
-    else
-        echo "cjdns is already running"
-        return 1
-    fi
+	if [ -z "$PID" ]; then
+		logger -t cjdns "Starting cjdns"
+		$CJDROUTE < $CONF
+		nat6 on
+		if [ $? -gt 0 ]; then
+			echo "Failed to start cjdns"
+			nat6 off
+			return 1
+		fi
+	else
+		echo "cjdns is already running"
+		return 1
+	fi
 }
+
