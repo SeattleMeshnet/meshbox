0b059aa2f3409b1773a5e7eee54d9f7d26666ed4
Lars Gierth <larsg@systemli.org>
2014-07-28 02:36:32 +0200

    Listen for network.interface

--- cjdns/lua/cjdns/mgmt.lua
+++ cjdns/lua/cjdns/mgmt.lua
@@ -6,5 +6,18 @@ Mgmt = {}
 common.mgmt = Mgmt
 
 function Mgmt.listen()
-  print("hahahaha")
+  uloop.init()
+
+  local conn = ubus.connect()
+  if not conn then
+    error("Failed to connect to ubus")
+  end
+
+  conn:listen({
+    ["network.interface"] = function(msg)
+      for k, v in pairs(msg) do print(k, v) end
+    end
+  })
+
+  uloop.run()
 end
