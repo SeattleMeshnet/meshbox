276b17fd2f491f2152b454d59a968d57d85f359d
Lars Gierth <larsg@systemli.org>
2014-08-19 12:44:04 +0200

    Add script for building Docker image

---
+++ build-scripts/docker-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Usage:
+#
+# cd openwrt/
+# feeds/meshbox/build-scripts/docker-image.sh
+# make
+# image_id=$(docker import - < bin/x86/openwrt-x86-generic-Generic-rootfs.tar.gz)
+# docker run -i -t $image_id /sbin/init
+
+# Or using the prebuilt image:
+#
+# docker run -i -t lgierth/meshbox /sbin/init
+
+rm .config
+make defconfig
+echo CONFIG_TARGET_x86=y >> .config
+#echo CONFIG_TARGET_x86_Default=y >> .config
+echo CONFIG_TARGET_ROOTFS_TARGZ=y >> .config
+echo CONFIG_PACKAGE_luci=y >> .config
+echo CONFIG_PACKAGE_luci-cjdns=y >> .config
+#echo CONFIG_PACKAGE_netperf=y >> .config
+#echo CONFIG_PACKAGE_tcpdump=y >> .config
+#echo CONFIG_PACKAGE_ip=y >> .config
+#echo CONFIG_PACKAGE_mtr=y >> .config
+#echo CONFIG_PACKAGE_iputils-ping=y >> .config
+#echo CONFIG_PACKAGE_iputils-tracepath=y >> .config
+#echo CONFIG_PACKAGE_iputils-ping6=y >> .config
+#echo CONFIG_PACKAGE_iputils-tracepath6=y >> .config
+#echo CONFIG_PACKAGE_iputils-traceroute6=y >> .config
+make defconfig
