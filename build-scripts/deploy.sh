b34fcd5a05aade9b242d34a1467ad0fdceff7b91
Lars Gierth <larsg@systemli.org>
2014-09-03 06:39:03 +0200

    build-scripts: fix password argument

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -2,32 +2,32 @@
 
 # Usage:
 # 1. docker run -i -t lgierth/meshbox /sbin/init
-# 2. set root password
+# 2. printf "12345\n12345\n" | passwd
 # 3. ifconfig eth0 | grep 'inet addr'
 # 3. coding
-# 4. build-scripts/deploy.sh root@ADDRESS THEPASSWORD
+# 4. build-scripts/deploy.sh root@ADDRESS 12345
 
 # # contrib/lua
-# sshpass -p 12345 scp \
+# sshpass -p $2 scp \
 #   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
 #   -r ../cjdns/contrib/lua/cjdns/* $0:/usr/lib/lua/cjdns/
 
 # root files
-sshpass -p 12345 scp \
+sshpass -p $2 scp \
   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   -r cjdns/files/* $1:/
 
 # Lua files
-sshpass -p 12345 scp \
+sshpass -p $2 scp \
   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   -r cjdns/lua/* $1:/usr/lib/lua/
 
 # LuCI files
-sshpass -p 12345 scp \
+sshpass -p $2 scp \
   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   -r luci-cjdns/luasrc/* $1:/usr/lib/lua/luci/
 
 # Purge LuCI cache
-sshpass -p 12345 ssh \
+sshpass -p $2 ssh \
   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   $1 'rm -r /tmp/luci-modulecache'
